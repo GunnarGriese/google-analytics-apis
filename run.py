68d3b88ef360a17253edcdb5410293a91a0fbc4d
milanIIH <milan@iihnordic.com>
2020-10-02 14:37:49 +0200

    test acc

--- run.py
+++ run.py
@@ -9,12 +9,13 @@ service_config = GoogleOAuth(
     CLIENT_SECRET_PATH="tools@iih.json",
     CREDENTIALS_PATH="creds.json",
     SERVICE_ACCOUNT_KEY_PATH=None,
-    SERVER_AUTH=True
+    SERVER_AUTH=True,
 )
 
 # Build service
 api_service = AnalyticsManagement(
-    service_config, account_id="152079293", property_id="UA-152079293-1", view_id="205698058")
+    service_config, account_id="130829", property_id="UA-130829-38", view_id="119395418"
+)
 
 # Get list of all accounts
 accounts, account_items = api_service.list_accounts()
